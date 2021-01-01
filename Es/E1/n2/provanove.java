db651622859a391e126458d3a323a70b1dc8a35e
BoboTheFlyingOwl <73617549+BoboTheFlyingOwl@users.noreply.github.com>
2021-01-01 15:22:40 +0100

    Update provanove.java

---
+++ Es/E1/n2/provanove.java
@@ -0,0 +1,27 @@
+
+public class provanove{
+  /* permutazioin in n, array 1,2 operandi
+  dim 2n ogni coppia
+  */
+
+  /* da stringa a int per il valore  */
+  /* Pre-requisiti:s rappresenta un intero
+    Post-condizioni: restituisce il valore intero corrispondente a s
+  */
+  public static int StringToInt(String a){
+  }
+
+  /*comparatore di numeri */
+  /* Pre-requisiti: a,b sono interi
+    Post-condizioni: restituisce True se a = b, falso altrimenti
+  */
+  public static boolean CompareValue(int a, int b){
+
+  }
+  /*permutazioni di tutte le molt */
+  /*
+    Post-condizioni: stampa a schermo [Consegna]
+  */
+  public static void Molt(int n){
+  }
+}
